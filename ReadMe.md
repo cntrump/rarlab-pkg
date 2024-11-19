ea167e5a0a062b880a8a1f8dc8b77bd121b3db7e
Lvv.me <me@lvv.me>
2024-11-19 16:51:48 +0800

    Update shell script

--- ReadMe.md
+++ ReadMe.md
@@ -3,6 +3,21 @@
 RAR version: `7.01`
 
 1. Download x64 and ARM versions of RAR for macOS from https://www.rarlab.com/download.htm
-2. Place the latest versions of `rar` and `unrar` files in the corresponding architecture's `rarlab/bin-${arch}` directory
+2. Place the latest versions of `rar` and `unrar` files in the corresponding architecture's `bin-${arch}` directory
 3. Put your license file `rarreg.key` in the root directory of this repository
 4. Execute the script `make-rarpkg.sh`, which will generate a universal format pkg installation package
+
+## Example
+
+```bash
+$ ./make-rarpkg.sh
+
+INFO: Rar version: 7.01
+INFO: rarreg.key not found, the created pkg installer will use trial license.
+INFO: rarlab/bin/rar found.
+INFO: rarlab/bin/unrar found.
+INFO: rarlab/etc/rarfiles.lst found.
+INFO: rarlab/lib/default.sfx found.
+pkgbuild: Inferring bundle components from contents of rarlab
+pkgbuild: Wrote package to Rar-7.01-macOS.pkg
+```
--- make-rarpkg.sh
+++ make-rarpkg.sh
@@ -2,22 +2,44 @@
 
 set -eu
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[38;5;208m'
+NC='\033[0m' # No Color
+
 rar_version=7.01
 rar_keyfile=rarreg.key
+
+echo "${GREEN}INFO${NC}: Rar version: ${rar_version}"
 
 [ -d rarlab/bin ] && rm -rf rarlab/bin
 
 mkdir rarlab/bin
 
-lipo -create rarlab/bin-arm/rar rarlab/bin-x64/rar -output rarlab/bin/rar
-lipo -create rarlab/bin-arm/unrar rarlab/bin-x64/unrar -output rarlab/bin/unrar
+lipo -create bin-arm/rar bin-x64/rar -output rarlab/bin/rar
+lipo -create bin-arm/unrar bin-x64/unrar -output rarlab/bin/unrar
 
 if [ ! -f ${rar_keyfile} ]; then
-  echo "INFO: ${rar_keyfile} not found, the created pkg installer will use trial license."
+  echo "${ORANGE}INFO${NC}: ${rar_keyfile} not found, the created pkg installer will use trial license."
 else
-  echo "INFO: found ${rar_keyfile}, it will be packaged into pkg and later installed in the /usr/local/lib."
+  echo "${GREEN}INFO${NC}: found ${rar_keyfile}, it will be packaged into pkg and later installed in the /usr/local/lib."
   cp -f ${rar_keyfile} rarlab/lib/
 fi
+
+function check_file() {
+  if [ -f $1 ]; then
+    echo "${GREEN}INFO${NC}: $1 found."
+    return 0
+  fi
+
+  echo "${RED}ERR${NC}: $0 not found!"
+  exit 1
+}
+
+check_file rarlab/bin/rar
+check_file rarlab/bin/unrar
+check_file rarlab/etc/rarfiles.lst
+check_file rarlab/lib/default.sfx
 
 pkgbuild --identifier com.rarlab.pkg.Rar \
          --version ${rar_version} \
